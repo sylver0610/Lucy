1419e592eb5b0be91f70283074754b4b904f1150
sylver0610 <79132069+sylver0610@users.noreply.github.com>
2023-04-11 22:29:28 +0300

    improve config and .env files

--- backend/src/configs/config.mongodb.js
+++ backend/src/configs/config.mongodb.js
@@ -17,23 +17,23 @@
 
 const dev = {
     app: {
-        port: 3000
+        port: process.env.DEV_APP_PORT || 3055
     },
     db: {
-        host: '127.0.0.1',
-        port: 27017,
-        name: 'shopDEV'
+        host: process.env.DEV_DB_HOST || '127.0.0.1',
+        port: process.env.DEV_DB_PORT || '27017',
+        name: process.env.DEV_DB_NAME || 'shopDEV'
     }
 }
 
 const production = {
     app: {
-        port: 3000
+        port: process.env.PRO_APP_PORT || 3052
     },
     db: {
-        host: '127.0.0.1',
-        port: 27017,
-        name: 'shopDEV_production'
+        host: process.env.PRO_DB_HOST || '127.0.0.1',
+        port: process.env.PRO_DB_PORT || '27017',
+        name: process.env.PRO_DB_NAME || 'shopPRO'
     }
 }
 
